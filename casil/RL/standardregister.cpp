f9227534dc4569ee37498c4f60c46ba8492323f2
M. Frohne <flooklab@users.noreply.github.com>
2026-04-11 15:39:53 +0200

    Update some missed copyright years.

--- casil/RL/standardregister.cpp
+++ casil/RL/standardregister.cpp
@@ -1,7 +1,7 @@
 /*
 //////////////////////////////////////////////////////////////////////////////////////////////////
 //
-//  Copyright (C) 2025 M. Frohne
+//  Copyright (C) 2025–2026 M. Frohne
 //
 //  This file is part of Casil, a reimplementation of the data acquisition framework basil in C++.
 //
--- casil/TL/Muxed/sitcp.cpp
+++ casil/TL/Muxed/sitcp.cpp
@@ -1,7 +1,7 @@
 /*
 //////////////////////////////////////////////////////////////////////////////////////////////////
 //
-//  Copyright (C) 2024–2025 M. Frohne
+//  Copyright (C) 2024–2026 M. Frohne
 //
 //  This file is part of Casil, a reimplementation of the data acquisition framework basil in C++.
 //

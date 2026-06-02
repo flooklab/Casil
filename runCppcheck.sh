dc1a63a38265669767bdcf7c9d8f2f9330189fd8
M. Frohne <flooklab@users.noreply.github.com>
2026-06-02 19:44:51 +0200

    Break long lines.

--- runCppcheck.sh
+++ runCppcheck.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-cppcheck --check-level=exhaustive --enable=all --std=c++23 --inline-suppr --suppress=missingIncludeSystem --suppress=unusedFunction -I ./ ./casil
-cppcheck --check-level=exhaustive --enable=all --std=c++23 --inline-suppr --suppress=missingIncludeSystem --suppress=unusedFunction -I ./ ./pycasil
-cppcheck --check-level=exhaustive --enable=all --std=c++23 --inline-suppr --suppress=missingIncludeSystem --suppress=unusedFunction -I ./ ./example/casil
-cppcheck --check-level=exhaustive --enable=all --std=c++23 --inline-suppr --suppress=missingIncludeSystem --suppress=unusedFunction --suppress=ctuOneDefinitionRuleViolation --library=boost -I ./ ./tests
+cppcheck --check-level=exhaustive --enable=all --std=c++23 --inline-suppr --suppress=missingIncludeSystem --suppress=unusedFunction \
+         -I ./ ./casil
+cppcheck --check-level=exhaustive --enable=all --std=c++23 --inline-suppr --suppress=missingIncludeSystem --suppress=unusedFunction \
+         -I ./ ./pycasil
+cppcheck --check-level=exhaustive --enable=all --std=c++23 --inline-suppr --suppress=missingIncludeSystem --suppress=unusedFunction \
+         -I ./ ./example/casil
+cppcheck --check-level=exhaustive --enable=all --std=c++23 --inline-suppr --suppress=missingIncludeSystem --suppress=unusedFunction \
+         --suppress=ctuOneDefinitionRuleViolation --library=boost -I ./ ./tests

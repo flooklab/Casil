230b52d11606d05f06760d010421b39766e1ec88
M. Frohne <flooklab@users.noreply.github.com>
2026-06-14 14:36:49 +0200

    Use nicer indentation.

--- example/pycasil/example.py
+++ example/pycasil/example.py
@@ -1,21 +1,21 @@
 ##################################################################################################
 #
-# Copyright (C) 2024–2025 M. Frohne
+#  Copyright (C) 2024–2025 M. Frohne
 #
-# This file is part of Casil, a reimplementation of the data acquisition framework basil in C++.
+#  This file is part of Casil, a reimplementation of the data acquisition framework basil in C++.
 #
-# Casil is free software: you can redistribute it and/or modify it
-# under the terms of the GNU Affero General Public License as published
-# by the Free Software Foundation, either version 3 of the License,
-# or (at your option) any later version.
+#  Casil is free software: you can redistribute it and/or modify it
+#  under the terms of the GNU Affero General Public License as published
+#  by the Free Software Foundation, either version 3 of the License,
+#  or (at your option) any later version.
 #
-# Casil is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty
-# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-# See the GNU Affero General Public License for more details.
+#  Casil is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty
+#  of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#  See the GNU Affero General Public License for more details.
 #
-# You should have received a copy of the GNU Affero General Public License
-# along with Casil. If not, see <https://www.gnu.org/licenses/>.
+#  You should have received a copy of the GNU Affero General Public License
+#  along with Casil. If not, see <https://www.gnu.org/licenses/>.
 #
 ##################################################################################################
 
